044cb58aaa7b1baec1bb912c012183809ec5b70e
Rithuik Yerrabrolu REDDY <rithuikyerrabrolu.x.rithuikyerrabrolu@haleon.com>
2023-05-11 20:25:39 +0530

    Added

---
+++ Blobs/src/main/java/com/azure/storage/App.java
@@ -0,0 +1,17 @@
+package com.azure.storage;
+
+import com.azure.storage.blob.BlobServiceClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //BlobServiceClient blobServiceClient = new BlobServiceClient().
+
+
+    }
+}
